fed43dc9c1e66f9d6f47d86fb6ef5a5531d7a6c8
John Bynum <iexclusiveproductions@gmail.com>
2022-09-29 17:15:59 -0400

    updating JS Website files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Engineer = require ('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern')
+const generateWebsite = require('./source/website');
 
