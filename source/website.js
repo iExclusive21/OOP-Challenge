f60d812feaed99226b710f19815c4c3d6f15e9da
John Bynum <iexclusiveproductions@gmail.com>
2022-10-13 22:49:11 -0400

    Finished writing the website generator syntax

--- source/website.js
+++ source/website.js
@@ -12,6 +12,41 @@ const generateWebsite = (team) => {
             <div class="row3">Office Number: ${manager.getOfficeNumber()}</div>
         </div>
     </div>
-        `
+        `;
+    };
+
+    const createEngineer = (engineer) => {
+        return `
+        <div class="titleCard1">
+        <div class="cardHead">
+            <h2>${engineer.getName()}</h2>
+            <div class="Manager">Manager</div>
+        </div>
+        <div class="midBox">
+            <div class="row1">ID:${engineer.getId()}></div>
+            <div class="row2">Email: ${engineer.getEmail()}></div>
+            <div class="row3">Office Number: ${engineer.getOfficeNumber()}</div>
+        </div>
+    </div>
+        `;
     }
+    const createIntern = (intern) => {
+        return `
+        <div class="titleCard1">
+        <div class="cardHead">
+            <h2>${intern.getName()}</h2>
+            <div class="Manager">Manager</div>
+        </div>
+        <div class="midBox">
+            <div class="row1">ID:${intern.getId()}></div>
+            <div class="row2">Email: ${intern.getEmail()}></div>
+            <div class="row3">Office Number: ${intern.getOfficeNumber()}</div>
+        </div>
+    </div>
+        `;
+    }
+
+
+    
 }
+
