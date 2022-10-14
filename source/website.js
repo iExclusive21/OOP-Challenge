afa146b218771c6a6426b4d3031c134f81bb88f4
John Bynum <iexclusiveproductions@gmail.com>
2022-10-13 22:55:05 -0400

    updated each class to mimic the html file

--- source/website.js
+++ source/website.js
@@ -17,36 +17,36 @@ const generateWebsite = (team) => {
 
     const createEngineer = (engineer) => {
         return `
-        <div class="titleCard1">
-        <div class="cardHead">
+        <div class="titleCard1B">
+        <div class="cardHeadB">
             <h2>${engineer.getName()}</h2>
             <div class="Manager">Manager</div>
         </div>
-        <div class="midBox">
-            <div class="row1">ID:${engineer.getId()}></div>
-            <div class="row2">Email: ${engineer.getEmail()}></div>
-            <div class="row3">Office Number: ${engineer.getOfficeNumber()}</div>
+        <div class="midBoxB">
+            <div class="row1B">ID:${engineer.getId()}></div>
+            <div class="row2B">Email: ${engineer.getEmail()}></div>
+            <div class="row3B">Office Number: ${engineer.getOfficeNumber()}</div>
         </div>
     </div>
         `;
     }
     const createIntern = (intern) => {
         return `
-        <div class="titleCard1">
-        <div class="cardHead">
+        <div class="titleCard1C">
+        <div class="cardHeadC">
             <h2>${intern.getName()}</h2>
             <div class="Manager">Manager</div>
         </div>
-        <div class="midBox">
-            <div class="row1">ID:${intern.getId()}></div>
-            <div class="row2">Email: ${intern.getEmail()}></div>
-            <div class="row3">Office Number: ${intern.getOfficeNumber()}</div>
+        <div class="midBoxC">
+            <div class="row1C">ID:${intern.getId()}></div>
+            <div class="row2C">Email: ${intern.getEmail()}></div>
+            <div class="row3C">Office Number: ${intern.getOfficeNumber()}</div>
         </div>
     </div>
         `;
     }
 
 
-    
+
 }
 
